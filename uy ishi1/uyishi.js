65a20b18025a5ba471857f088bd0fa11a6fbe1d0
Ilyos-lab <yangiakkaunt234@gmail.com>
2026-03-03 10:45:58 +0500

    New symbles

--- uy ishi1/uyishi.js
+++ uy ishi1/uyishi.js
@@ -11,3 +11,40 @@ if (ball >= 60 && davomat >= 70) {
 } else {
   console.log("Siz keyingi bosqichga o'tmadingiz");
 }
+
+
+
+var a = "string";
+
+var isHareCar = false;
+
+var b = 10;
+
+var  biggerINter = 8709478092347893478943789347893028475937495783490572394758934759837459837458973894n;
+
+var p = null;
+
+var sy = Symbol("fhuwef")
+
+console.log("a, isHareCar, b, biggerINter, p, sy,");
+
+
+var Asadbek = {
+    name: "Asadbek",
+    age: 13,
+    IsHascar :false,
+    isStudent: true,
+    school: 30,
+};
+
+console.log("Asadbek");
+
+var array = [
+    "asadbek",
+    13,
+    false,
+    true,
+    30,
+]
+
+console.log(array);
