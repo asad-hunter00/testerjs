f72117e90c9b1e1ffad82e3429f4d2afdbad9fcb
Ilyos-lab <yangiakkaunt234@gmail.com>
2026-03-07 11:31:35 +0500

    new dayProject

--- uy ishi1/whiletest.js
+++ uy ishi1/whiletest.js
@@ -1,10 +1,99 @@
-let i = 0;
+var a = prompt("Oyingizni ayting!");
 
+switch (a) {
+  case "1":
+    console.log("yanvar");
 
-while (i <= 100) {
-    if (i % 2 == 0) {
-        console.log(i)
-    }
+    break;
 
-    i++;
+  case "yanvar":
+    console.log("1");
+
+  case "2":
+    console.log("fevral");
+
+    break;
+
+  case "fevral":
+    console.log("2");
+
+  case "3":
+    console.log("mart");
+
+    break;
+
+  case "mart":
+    console.log("3");
+
+  case "4":
+    console.log("aprel");
+
+    break;
+
+  case "aprel":
+    console.log("4");
+
+  case "5":
+    console.log("may");
+
+    break;
+
+  case "may":
+    console.log("5");
+
+  case "6":
+    console.log("iyul");
+
+    break;
+
+  case "iyul":
+    console.log("6");
+
+  case "7":
+    console.log("iyun");
+
+    break;
+
+  case "iyun":
+    console.log("7");
+
+  case "8":
+    console.log("avgust");
+
+    break;
+
+  case "avgust":
+    console.log("8");
+
+  case "9":
+    console.log("sentyabr");
+
+    break;
+
+  case "sentyabr":
+    console.log("9");
+
+  case "10":
+    console.log("oktyabr");
+
+    break;
+
+  case "oktyabr":
+    console.log("10");
+
+  case "11":
+    console.log("noyabr");
+
+    break;
+
+  case "noyabr":
+    console.log("11");
+
+  case "12":
+    console.log("dekabr");
+
+    break;
+
+  case "dekabr":
+    console.log("12");
 }
