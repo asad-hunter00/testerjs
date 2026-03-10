857540098ff5e30987d6173e08cd198a1ea90b4e
AsadbekDEV <asadbekyakubov08@gmail.com>
2026-03-10 07:38:39 +0300

    new calculator

--- uy ishi1/whiletest.js
+++ uy ishi1/whiletest.js
@@ -1,10 +1,10 @@
-let i = 0;
+// let i = 0;
 
 
-while (i <= 100) {
-    if (i % 2 == 0) {
-        console.log(i)
-    }
+// while (i <= 100) {
+//     if (i % 2 == 0) {
+//         console.log(i)
+//     }
 
-    i++;
-}
+//     i++;
+// }
