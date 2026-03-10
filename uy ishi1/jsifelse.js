e7b20924e94384f102ef4a833cc0f52c410a391d
Ilyos-lab <yangiakkaunt234@gmail.com>
2026-03-10 11:44:03 +0500

    source jsmedoths

---
+++ uy ishi1/jsifelse.js
@@ -0,0 +1,147 @@
+// if else qisqaroq varianti
+
+// var a = prompt("son yozing");
+
+// console.log(a > 30 ? "xush kelibsiz" : "kichkinasiz");
+
+
+
+// length
+
+
+let text = "HELLO WORLD";
+
+console.log(text.length);
+
+
+// chartAT
+
+let text2 = "salom dunyo";
+console.log(text2.charAt(0));
+
+// CHARCODEAT
+let text3 = "hello world";
+
+console.log(text3.charCodeAt(0));
+
+// codePointAt
+
+let text4 = "hello world";
+
+console.log(text.codePointAt(0));
+
+// concat
+
+let text5 = "hello";
+let text6 = "world";
+console.log(text5.concat("", text6));
+
+// at
+
+let text7 = "hello world";
+
+console.log(text7.at(0));
+
+// [...]
+
+let text8 = "hellow world";
+
+console.log([...text8]);
+
+// slice
+
+let text9 = "hello world";
+
+console.log(text9.slice(0, 5));
+
+// substring
+
+let text10 = "hello world";
+
+console.log(text10.substring(0, 5));
+
+
+// substr
+
+let text11 = "hello wolrd";
+
+console.log(text11.substr(0, 5));
+
+// toUpperCase
+
+let text12 = "hello world";
+
+console.log(text12.toLocaleUpperCase());
+
+// toLowerCase
+
+let text13 = "hello world";
+console.log(text13.toLocaleLowerCase());
+
+// isWellFormed
+
+let text14 = "hello world";
+
+console.log(text14.isWellFormed());
+
+// toWellFormed
+
+let text15 = "hello world";
+
+console.log(text15.toWellFormed());
+
+// trim
+
+let text16 = "               hello world           ";
+console.log(text16.trim());
+
+// trimStart
+
+let text17 = "                        hello world";
+
+console.log(text17.trimStart());
+
+// trimEnd
+
+let text18 = "hello world                ";
+
+console.log(text18.trimEnd());
+
+// padStart
+
+let text19 = "hello world";
+
+
+
+console.log(text19.padStart(20, "&"));
+
+// padEnd
+
+
+let text20 = "hello world";
+
+console.log(text20.padEnd(EncodedAudioChunk, "&"));
+
+// repeat
+
+
+let text21 = "hello world";
+
+console.log(text21.repeat(3));
+
+// replace
+
+let text22 = "hello world";
+console.log(text22.replace("hello", "hi"));
+
+// replaceALL
+
+let text23 = "hello world hello";
+
+console.log(text23.replaceAll(hello, "hi"));
+
+// split
+
+let text24 = "hello world";
+
+console.log(text24.split(" "));
