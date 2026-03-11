2d25ac747c4d5dea7bdd685309a66cb04831e901
AsadbekDEV <asadbekyakubov08@gmail.com>
2026-03-11 21:21:30 +0300

    new string medotspr

--- uy ishi1/calculator.js
+++ uy ishi1/calculator.js
@@ -1,24 +1,24 @@
-let var1 = +prompt("Birinchi sonni kiriting")
-let var2 = +prompt("Ikkinchi sonni kiriting")
-let op = prompt("Amalni kiriting: +, -, *, /")
+// let var1 = +prompt("Birinchi sonni kiriting")
+// let var2 = +prompt("Ikkinchi sonni kiriting")
+// let op = prompt("Amalni kiriting: +, -, *, /")
 
-let result
+// let result
 
-switch(op){
-  case "+":
-    result = var1 + var2
-    break
-  case "-":
-    result = var1 - var2
-    break
-  case "*":
-    result = var1 * var2
-    break
-  case "/":
-    result = var1 / var2
-    break
-  default:
-    result = "Noto'g'ri amal"
-}
+// switch(op){
+//   case "+":
+//     result = var1 + var2
+//     break
+//   case "-":
+//     result = var1 - var2
+//     break
+//   case "*":
+//     result = var1 * var2
+//     break
+//   case "/":
+//     result = var1 / var2
+//     break
+//   default:
+//     result = "Noto'g'ri amal"
+// }
 
-console.log(result)
+// console.log(result);
