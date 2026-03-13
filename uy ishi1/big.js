625708be4e036e14b553a09fdb72570984456dbe
AsadbekDEV <asadbekyakubov08@gmail.com>
2026-03-13 20:59:26 +0300

    testerjs newcommits

--- uy ishi1/big.js
+++ uy ishi1/big.js
@@ -1,12 +1,12 @@
-let str = prompt("So'z yozing");
+// let str = prompt("BU yerga yozing");
 
-console.log("Birinchi harf:", str.charAt(0));
+// console.log("Birinchi harf:", str.charAt(0));
 
-console.log("3 5 gacha:", str.slice(2,5));
-console.log("uzunligi:", str.length);
+// console.log("3 5 gacha:", str.slice(2,5));
+// console.log("uzunligi:", str.length);
 
-console.log("katta harf:",  str.toUpperCase());
+// console.log("katta harf:",  str.toUpperCase());
 
-console.log("kchik harf:", str.toLowerCase());
-console.log("Bo'sh joysiz:", str.trim());
-console.log("hi " + str);
+// console.log("kchik harf:", str.toLowerCase());
+// console.log("Bo'sh joysiz:", str.trim());
+// console.log("hi " + str);
---
+++ uy ishi1/warning.js
@@ -0,0 +1,14 @@
+let war1 = prompt("Qaysi tilni bilasiz?");
+
+
+if(war1.includes("react")) {
+    console.log("web Developer");
+}
+else if (war1.includes("nodejs")) {
+    console.log("Backend Developer");
+}
+
+
+else if (war1.includes("pyhton")) {
+    console.log("Ai developer");
+}
