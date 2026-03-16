01e3ff810cd8e210f42ee01be215de03d08efdd9
AsadbekDEV <asadbekyakubov08@gmail.com>
2026-03-16 21:19:44 +0300

    firstpoject in js

--- uy ishi1/warning.js
+++ uy ishi1/warning.js
@@ -1,14 +1,14 @@
-let war1 = prompt("Qaysi tilni bilasiz?");
+// let war1 = prompt("Qaysi tilni bilasiz?");
 
 
-if(war1.includes("react")) {
-    console.log("web Developer");
-}
-else if (war1.includes("nodejs")) {
-    console.log("Backend Developer");
-}
+// if(war1.includes("react")) {
+//     console.log("web Developer");
+// }
+// else if (war1.includes("nodejs")) {
+//     console.log("Backend Developer");
+// }
 
 
-else if (war1.includes("pyhton")) {
-    console.log("Ai developer");
-}
+// else if (war1.includes("pyhton")) {
+//     console.log("Ai developer");
+// }
