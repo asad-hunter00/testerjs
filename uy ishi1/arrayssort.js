31d601465c09deaf2ab7e478fe585aecd7de1410
Ilyos-lab <yangiakkaunt234@gmail.com>
2026-03-19 11:52:47 +0500

    sort theme

---
+++ uy ishi1/arrayssort.js
@@ -0,0 +1,58 @@
+// var arr = [167, 190, 129, 6752, 9876];
+
+// arr.sort(function (a, b) {
+//     return a + b;
+// });
+
+// console.log(arr);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// console.log(fruits.reverse());
+
+// let len1 = prompt("arrays kirgazing");
+
+// let len2 = prompt("qaqanaqa shaklda kirgazing masalan; A-Z, Z-A, 1-9, 9-1");
+
+// let arr = len1.split(" ");
+
+// arr.sort(function (a, b) {
+//   return b - a;
+// });
+
+
+
+// if (len2.toLowerCase().trim() == "a-z") {
+//   console.log(arr.sort());
+// } else if (len2.toLowerCase().trim() == "z-a") {
+//   console.log(arr.sort().reverse());
+// } else if (len2 == "9-1") {
+//   console.log(
+//     arr.sort(function (a, b) {
+//       return b - a;
+//     }),
+//   );
+// } else if (len2 == "1-9") {
+//   console.log(
+//     arr.sort(function (a, b) {
+//       return a - b;
+//     }),
+//   );
+// }
+
+
+
+
+
+// var arrayOfNumber = [
+//     {name: "asadbek", age: 48 },
+//     {name: "shohjahon", age: 34},
+//     {name: "behruz", age: 14},
+// ];
+
+
+//     arrayOfNumber.sort(function (a, b) {
+//         return b.age - a.age;
+//     });
+
+// console.log(arrayOfNumber);
---
+++ uy ishi1/functionc.js
@@ -0,0 +1,26 @@
+// function calc() {
+//   let str = prompt("misol kirgazing");
+
+//   let arr = str.split(" ");
+
+//   let a = Number(arr[0]);
+
+
+// // massivlar
+//   let op = arr[1];
+//   let b = Number(arr[2]);
+
+//   //  tkshiruv
+//   if (op === "+") {
+//     console.log(a + b);
+//   } else if (op === "-") {
+//     console.log(a - b);
+//   } else if (op === "*") {
+//     console.log(a * b);
+//   } else if (op === "/") {
+//     console.log(a / b);
+//   } else {
+//     console.log("xato amal bajardingiz.");
+//   }
+// }
+// calc();
