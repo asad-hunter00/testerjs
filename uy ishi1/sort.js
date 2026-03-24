8ae46b8e05846ec726edac25fb03df3cc6eff05f
AsadbekDEV <asadbekyakubov08@gmail.com>
2026-03-24 07:35:16 +0300

    sort calculator

---
+++ uy ishi1/sort.js
@@ -0,0 +1,28 @@
+
+
+
+var students = [
+    { name: "Ali", score: 85 },
+    { name: "Zafar", score: 92 },
+    { name: "Sardor", score: 85 },
+    { name: "Bobur", score: 70 }
+];
+
+
+let name = prompt("qaysi boyicha sort qilasiz name/score");
+
+
+if (name === "name") {
+    students.sort((a, b) => a.name.localeCompare(b.name))
+}   else if (name === "score") {
+     students.sort((a, b) => a.score - b.score);
+}  else {
+    console.log("notogri kiritingiz")
+
+}
+
+
+console.log(students);
+
+
+
