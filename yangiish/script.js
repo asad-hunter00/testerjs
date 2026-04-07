6c9522151534a17d68075b36a604b3754b94c665
AsadbekDEV <asadbekyakubov08@gmail.com>
2026-04-07 08:25:25 +0300

    theme types

---
+++ yangiish/script.js
@@ -0,0 +1,135 @@
+// Immutable (O'zgarmas)
+
+// Primitive Types 
+
+// Number 
+
+let a = 10;
+let b = a; 
+
+a = 20;
+
+console.log(a)
+
+
+
+// string 
+
+let str = "hello";
+
+let str2 = str;
+
+str2 = "world";
+
+console.log(str2)
+
+
+// boolean
+
+
+let isTrue = true;
+let check = isTrue;
+
+
+check = false;
+
+console.log(check);
+
+
+
+
+// null
+
+let x = null;
+
+let y = x;
+
+y = 5;
+
+console.log(y)
+
+
+
+// undifined
+
+let fined
+
+let fined2 = fined;
+
+fined2 = 100;
+
+console.log(fined2);
+
+
+
+
+
+// Mutable (O'zgaruvchan)
+
+// Reference types
+
+
+// array
+
+let arr1 = [1,2,3]
+let arr2 = arr1
+
+arr2[0] = 99
+
+console.log(arr1)
+
+
+
+// object
+
+
+let user1 = {name: "Ali"}
+let user2 = user1
+
+user2.name = "Vali"
+
+console.log(user1.name)
+
+
+// function
+
+
+let f1 = function() { return "hello" }
+let f2 = f1
+
+console.log(f2())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// “Reference type mutable, lekin agar biz uni qayta yangi qiymatga tenglasak — bu mutation emas, bu reassignment”
+
+
+
+
+
+// **“bugun o‘tadigan mavzumiz — mutable va immutable.
+// Bu so‘zlar o‘zgaruvchan va o‘zgarmas degan ma’noni anglatadi.
+// 
+// JavaScript’da ma’lumotlar 2 turga bo‘linadi:
+// primitive types va reference types.
+// 
+// Primitive typelarga:
+// number, string, boolean, null, undefined kiradi.
+// 
+// Reference typelarga esa:
+// object, array va function kiradi.
+// 
+// Endi keling, buni VS Code’da misollar bilan ko‘rib chiqamiz.”**
