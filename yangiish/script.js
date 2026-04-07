9b97fb957c568aaa7bfeb860f73c4ed89acd32ee
Ilyos-lab <yangiakkaunt234@gmail.com>
2026-04-07 12:04:16 +0500

    theme repeat

--- yangiish/script.js
+++ yangiish/script.js
@@ -1,103 +1,156 @@
-// Immutable (O'zgarmas)
+// // Immutable (O'zgarmas)
 
-// Primitive Types 
+// // Primitive Types
 
-// Number 
+// // Number
 
-let a = 10;
-let b = a; 
+// let a = 10;
+// let b = a;
 
-a = 20;
+// a = 20;
 
-console.log(a)
+// console.log(a)
 
+// // string
 
+// let str = "hello";
 
-// string 
+// let str2 = str;
 
-let str = "hello";
+// str2 = "world";
 
-let str2 = str;
+// console.log(str2)
 
-str2 = "world";
+// // boolean
 
-console.log(str2)
+// let isTrue = true;
+// let check = isTrue;
 
+// check = false;
 
-// boolean
+// console.log(check);
 
+// // null
 
-let isTrue = true;
-let check = isTrue;
+// let x = null;
 
+// let y = x;
 
-check = false;
+// y = 5;
 
-console.log(check);
+// console.log(y)
 
+// // undifined
 
+// let fined
 
+// let fined2 = fined;
 
-// null
+// fined2 = 100;
 
-let x = null;
+// console.log(fined2);
 
-let y = x;
+// // Mutable (O'zgaruvchan)
 
-y = 5;
+// // Reference types
 
-console.log(y)
+// // array
 
+// let arr1 = [1,2,3]
+// let arr2 = arr1
 
+// arr2[0] = 99
 
-// undifined
+// console.log(arr1)
 
-let fined
+// // object
 
-let fined2 = fined;
+// let user1 = {name: "Ali"}
+// let user2 = user1
 
-fined2 = 100;
+// user2.name = "Vali"
 
-console.log(fined2);
+// console.log(user1.name)
 
+// // function
 
+// let f1 = function() { return "hello" }
+// let f2 = f1
 
-
-
-// Mutable (O'zgaruvchan)
-
-// Reference types
-
-
-// array
-
-let arr1 = [1,2,3]
-let arr2 = arr1
-
-arr2[0] = 99
-
-console.log(arr1)
-
-
-
-// object
+// console.log(f2())
 
 
-let user1 = {name: "Ali"}
-let user2 = user1
-
-user2.name = "Vali"
-
-console.log(user1.name)
 
 
-// function
 
 
-let f1 = function() { return "hello" }
-let f2 = f1
+// var son = {
+//   name: "Saloh",
+//   age: 13,
+// };
 
-console.log(f2())
+
+
+// var son2 = son;
+
+// son2.name = "Farruh";
+
+// console.log(son);
+
+
+
+const Arr = ["Bmw", "Porsche", "Supra,"];
+let add = prompt("Bir mashina Nomini yozing");
+
+
+
+
+console.log(Arr + add)
+
+
+
+
+
+
+
+// var myHobby = "Code";
+// var myHobby = "scinence";
+// myHobby = "Javascript";
+
+
+// let cars = "Porsche";
+// cars =  "Bmw";
+
+
+// const math = "Information";
+// math = "Foundation";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -116,20 +169,16 @@ console.log(f2())
 
 // “Reference type mutable, lekin agar biz uni qayta yangi qiymatga tenglasak — bu mutation emas, bu reassignment”
 
-
-
-
-
 // **“bugun o‘tadigan mavzumiz — mutable va immutable.
 // Bu so‘zlar o‘zgaruvchan va o‘zgarmas degan ma’noni anglatadi.
-// 
+//
 // JavaScript’da ma’lumotlar 2 turga bo‘linadi:
 // primitive types va reference types.
-// 
+//
 // Primitive typelarga:
 // number, string, boolean, null, undefined kiradi.
-// 
+//
 // Reference typelarga esa:
 // object, array va function kiradi.
-// 
+//
 // Endi keling, buni VS Code’da misollar bilan ko‘rib chiqamiz.”**
