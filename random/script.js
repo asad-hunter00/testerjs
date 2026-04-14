6f14175103607e5ddac09b6cb3a1548ebec023c1
Ilyos-lab <yangiakkaunt234@gmail.com>
2026-04-14 11:27:46 +0500

    calc

--- random/script.js
+++ random/script.js
@@ -1,12 +1,30 @@
-let a = document.getElementById("input");
-let result = document.getElementById("result");
+// let a = document.getElementById("input");
+// let result = document.getElementById("result");
 
-function random() {
-  result.innerHTML = "";
+// function random() {
+//   result.innerHTML = "";
 
-  for (let i = 0; i < a; i++) {
-    let r = Math.floor(Math.random() * number.length);
-  }
+//   for (let i = 0; i < a; i++) {
+//     let r = Math.floor(Math.random() * number.length);
+//   }
+// }
+
+// random();
+
+function genearetNum() {
+
+  let num = Math.trunc(Math.random * 10000000)
+
+  return `+99891${num.toString().padEnd(7, 0)}`;
+  
+
+ 
 }
 
-random();
+console.log(genearetNum());
+
+
+function name(params) {
+  
+}
+
