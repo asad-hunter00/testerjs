8828d44a7f5dc851b2ce8405c527214a1a60cb5d
AsadbekDEV <asadbekyakubov08@gmail.com>
2026-04-14 07:05:58 +0300

    theme random

---
+++ random/script.js
@@ -0,0 +1,13 @@
+let a = document.getElementById("input");
+let result = document.getElementById("result");
+
+function random() {
+    result.innerHTML = "";
+
+    for (let i = 0; i < a; i++) {
+        let r = Math.floor(Math.random() * number.length);
+        result.innerHTML += number[r];
+    }
+}
+
+random();
